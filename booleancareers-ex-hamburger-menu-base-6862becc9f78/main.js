777f60d595aab92ba0b141e9bf44c3a46d8c04b5
Nicola <nicolamelito.11@gmail.com>
2020-11-02 15:56:30 +0100

    Ex. completato
    sia come fatto osservare da Flavio che da Alessandro

---
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -0,0 +1,29 @@
+// apro l'humburger menu
+
+$('.header-right > a').click(function() {
+  $('.hamburger-menu').fadeIn(2000);
+})
+
+// chiudo l'hamburger menu
+
+$('.close').click(function() {
+  $('.hamburger-menu').fadeOut('fast');
+})
+
+
+/* in realtà io avevo eseguito l'esercizio come eseguito stamattina da Flavio
+ma vista la richiesta di Alessandro commenteroò questo per fare come desidera lui
+*/
+
+
+//apro l'Hamburger
+
+//$('.header-right > a').click(function() {
+//  $('.hamburger-menu').show();
+//})
+
+// chiudo l'hamburger
+
+//$('.hamburger-menu > .close').click(function() {
+//  $('.hamburger-menu').hide();
+//})
