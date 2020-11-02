fdc61a36cade9e479c9de1e91894dbb2e51d5f91
Nicola <nicolamelito.11@gmail.com>
2020-11-02 17:36:47 +0100

    Aggiornamento dell'Ex
    usato addClass e removeClass

--- booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -1,14 +1,14 @@
 // apro l'humburger menu
 
-$('.header-right > a').click(function() {
-  $('.hamburger-menu').fadeIn(2000);
-})
+//$('.header-right > a').click(function() {
+//  $('.hamburger-menu').fadeIn(2000);
+//})
 
 // chiudo l'hamburger menu
 
-$('.close').click(function() {
-  $('.hamburger-menu').fadeOut('fast');
-})
+//$('.close').click(function() {
+//  $('.hamburger-menu').fadeOut('fast');
+//})
 
 
 /* in realtà io avevo eseguito l'esercizio come eseguito stamattina da Flavio
@@ -27,3 +27,15 @@ ma vista la richiesta di Alessandro commenteroò questo per fare come desidera l
 //$('.hamburger-menu > .close').click(function() {
 //  $('.hamburger-menu').hide();
 //})
+
+
+// svolgiamolo come fatto osservare ancora una volta da Alessandro
+
+// apro menu
+$('.header-right > a').click(function() {
+  $('.hamburger-menu').addClass('active');
+})
+// chiudo menu
+$('.close').click(function() {
+  $('.hamburger-menu').removeClass('active');
+})
